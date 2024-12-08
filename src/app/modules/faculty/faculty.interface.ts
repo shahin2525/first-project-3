8eb3545408a5a6f4d5218a7a952167e06bdc11e0
shahin2525 <shahin451999@gmail.com>
2024-12-08 09:55:46 -0800

    add faculty model

---
+++ src/app/modules/faculty/faculty.interface.ts
@@ -0,0 +1,43 @@
+import { Model, Types } from 'mongoose';
+
+export type TGender = 'male' | 'female' | 'other';
+export type TBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
+export type TFacultyName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type TFaculty = {
+  id: string;
+  user: Types.ObjectId;
+  designation: string;
+  name: TFacultyName;
+  gender: TGender;
+  dateOfBirth?: Date;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImg?: string;
+  academicDepartment: Types.ObjectId;
+  isDeleted: boolean;
+};
+
+export interface FacultyModel extends Model<TFaculty> {
+  doesUserExists(): Promise<TFaculty | null>;
+}
+export interface FacultyModel extends Model<TFaculty> {
+  doesNotUserExists(): Promise<TFaculty | null>;
+}
