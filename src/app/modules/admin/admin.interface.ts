c9e255d48bdd60fb003ab655d89fac8bd8438e9a
shahin2525 <shahin451999@gmail.com>
2024-12-09 16:24:02 -0800

    add admin

---
+++ src/app/modules/admin/admin.interface.ts
@@ -0,0 +1,44 @@
+import { Model, Types } from 'mongoose';
+
+export type TGender = 'male' | 'female' | 'other';
+export type TBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
+export type TAdminName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type TAdmin = {
+  id: string;
+  user: Types.ObjectId;
+  designation: string;
+  name: TAdminName;
+  gender: TGender;
+  dateOfBirth?: Date;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  profileImg?: string;
+  isDeleted: boolean;
+};
+
+export interface AdminModel extends Model<TAdmin> {
+  // eslint-disable-next-line no-unused-vars
+  isUserExists(id: string): Promise<TAdmin | null>;
+}
+export interface AdminModel extends Model<TAdmin> {
+  // eslint-disable-next-line no-unused-vars
+  UserDoesNotExists(id: string): Promise<TAdmin | null>;
+}
