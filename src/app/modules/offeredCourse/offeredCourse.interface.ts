1be8967f87e590c171a31d73b6328c2718079683
shahin2525 <shahin451999@gmail.com>
2024-12-14 11:04:50 -0800

    add offered course

---
+++ src/app/modules/offeredCourse/offeredCourse.interface.ts
@@ -0,0 +1,22 @@
+import { Types } from 'mongoose';
+export type Days =
+  | 'Sunday'
+  | 'Monday'
+  | 'Tuesday'
+  | 'Wednesday'
+  | 'Thursday'
+  | 'Friday'
+  | 'Saturday';
+export type TOfferedCourse = {
+  semesterRegistration: Types.ObjectId;
+  academicSemester: Types.ObjectId;
+  academicFaculty: Types.ObjectId;
+  academicDepartment: Types.ObjectId;
+  course: Types.ObjectId;
+  faculty: Types.ObjectId;
+  maxCapacity: number;
+  section: number;
+  days: Days;
+  startTime: string;
+  endTime: string;
+};
---
+++ src/app/modules/offeredCourse/offeredCourse.model.ts
@@ -0,0 +1,47 @@
+import { model, Schema } from 'mongoose';
+import { TOfferedCourse } from './offeredCourse.interface';
+import { Days } from './offeredCourse.const';
+
+const OfferedCourseSchema = new Schema<TOfferedCourse>(
+  {
+    semesterRegistration: {
+      type: Schema.Types.ObjectId,
+      ref: 'SemesterRegistration',
+      required: true,
+    },
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+      required: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+      required: true,
+    },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicDepartment',
+      required: true,
+    },
+    course: { type: Schema.Types.ObjectId, ref: 'Course', required: true },
+    faculty: { type: Schema.Types.ObjectId, ref: 'Faculty', required: true },
+    maxCapacity: { type: Number, required: true },
+    section: { type: Number, required: true },
+    days: {
+      type: String,
+      enum: Days,
+      required: true,
+    },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const OfferedCourseModel = model<TOfferedCourse>(
+  'OfferedCourse',
+  OfferedCourseSchema,
+);
