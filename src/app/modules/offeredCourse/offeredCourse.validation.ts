e2757161a1b9862afc1099158c064a7a3cce049b
shahin2525 <shahin451999@gmail.com>
2024-12-14 21:25:12 -0800

    add time regex

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 import { Days } from './offeredCourse.const';
-
+const timeRegex = /^([01]?\d|2[0-3]):([0-5]\d)$/;
 const createOfferedCourseValidationSchema = z.object({
   body: z.object({
     semesterRegistration: z.string(),
@@ -13,8 +13,8 @@ const createOfferedCourseValidationSchema = z.object({
     maxCapacity: z.number(),
     section: z.number(),
     days: z.array(z.enum([...Days] as [string, ...string[]])),
-    startTime: z.string(),
-    endTime: z.string(),
+    startTime: z.string().regex(timeRegex, 'time is not format'),
+    endTime: z.string().regex(timeRegex, 'time is not format'),
   }),
 });
 // update offered corse schema
