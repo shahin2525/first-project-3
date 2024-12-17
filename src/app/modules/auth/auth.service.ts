fdd029501f15e320ee9a603aaf96cdf60b7c4e85
shahin2525 <shahin451999@gmail.com>
2024-12-17 10:02:16 -0800

    add compare bcript

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,37 @@
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../error/appError';
+import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
-
+import bcrypt from 'bcryptjs';
 const loginUser = async (payload: TLoginUser) => {
   console.log(payload);
+  //   check user
+  const isUserExists = await User.findOne({
+    id: payload?.id,
+  });
+  if (!isUserExists) {
+    throw new AppError(StatusCodes.NOT_FOUND, 'user not found');
+  }
+  // check is deleted true
+
+  const isUserDeleted = isUserExists?.isDeleted;
+  if (isUserDeleted) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'user is deleted');
+  }
+  //   is user blocked
+
+  const isUserBlocked = isUserExists?.status === 'blocked';
+  if (isUserBlocked) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'user is blocked');
+  }
+  const isPasswordMatch = await bcrypt.compare(
+    payload?.password,
+    isUserExists?.password,
+  );
+  console.log(isPasswordMatch);
+  if (!isPasswordMatch) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'password is not match');
+  }
   return {};
 };
 
