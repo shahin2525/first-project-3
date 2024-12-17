a1777a953b83c015e215eb0561dfb130ecccbfcc
shahin2525 <shahin451999@gmail.com>
2024-12-17 11:31:03 -0800

    add user statics emthod

--- src/app/modules/auth/auth.interface.ts
+++ src/app/modules/auth/auth.interface.ts
@@ -1,4 +1,10 @@
+import { Model } from 'mongoose';
+
 export type TLoginUser = {
   id: string;
   password: string;
 };
+
+export interface UserModel extends Model<TLoginUser> {
+  isUserExists(id: string): number;
+}
